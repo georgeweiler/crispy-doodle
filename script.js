2957880d014e6d1f98c29521cf3fa3f612fd3906
Lee Tsai <lee.x.tsai@gmail.com>
2016-07-25 13:38:05 -0700

    [Feature] Remove enemy (single circle) upon collision

--- script.js
+++ script.js
@@ -7,16 +7,16 @@ function randomColor() {
     return color;
 }
 
-var h = 1000
-var w = 1000
+var h = 500
+var w = 500
 
-var nodes = d3.range(3).map(function() { 
+var nodes = d3.range(10).map(function() {
   return {
-    r: 20,
+    r: 5,
     cx: Math.random() * h,
     cy: Math.random() * w,
     fill: randomColor()
-  }; 
+  };
 })
 
 var svg = d3.select('body').append('svg')
@@ -37,7 +37,7 @@ dots.data(nodes)
   .attr('fill', function(d){return d.fill})
 
 var userRadius = function(){
-  return 50
+  return 25
 }
 
 var user = svg.select('.user')
@@ -46,35 +46,29 @@ var user = svg.select('.user')
     // .attr('cy', function(d){return d.cy})
     // .attr('fill', randomColor())
 
-d3.select('svg').on("mousemove", function () { 
+d3.select('svg').on("mousemove", function () {
   var userCoords = d3.mouse(this),
       uX = userCoords[0],
       uY = userCoords[1]
- 
+
   user.transition()
   .duration(5).ease('linear')
     .attr('cx', uX)
     .attr('cy', uY);
 
   var enemies = svg.selectAll('.enemy')
-  enemies[0].forEach(function(enemy){
-    eX = enemy.cx
-    eY = enemy.cy
+  console.log(enemies[0].slice(0));
+  var enemiesOnly = enemies[0].slice(0);
+  enemiesOnly.forEach(function(enemy){
+    // console.log("enemy: " + enemy.cx.baseVal.value);
+    eX = enemy.cx.baseVal.value
+    eY = enemy.cy.baseVal.value
     if(checkCollision(uX, uY, eX, eY, userRadius())){
-     console.log("COLISION!")
-     // enemies.exit().remove()
-    } 
+     enemy.remove();
+    }
   })
  });
 
 function checkCollision(uX, uY, eX, eY, r){
-  return Math.pow((uX - eX),2) + Math.pow((uY - eY),2)  < Math.pow(r, 2)  
+  return Math.pow((uX - eX),2) + Math.pow((uY - eY),2) < Math.pow(r, 2)
 }
-
-
-
-
-
-
-
-
