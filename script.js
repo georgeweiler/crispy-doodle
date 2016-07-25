4d8c8556ff69eefa013d8e1414848f527267a38f
Lee Tsai <lee.x.tsai@gmail.com>
2016-07-25 14:51:22 -0700

    [WIP] Random dot movement

--- script.js
+++ script.js
@@ -10,14 +10,20 @@ function randomColor() {
 var h = 500
 var w = 500
 
-var nodes = d3.range(10).map(function() {
+var nodes = d3.range(50).map(function() {
+
   return {
-    r: 5,
+    r: Math.random() * 15 + 5,
     cx: Math.random() * h,
     cy: Math.random() * w,
-    fill: randomColor()
+    fill: randomColor(),
+    transition: randomPosition()
   };
 })
+
+function randomPosition() {
+  return {x: Math.random() * w, y: Math.random() * h};
+}
 
 var svg = d3.select('body').append('svg')
   .attr('height', h)
@@ -35,6 +41,19 @@ dots.data(nodes)
   .attr('cy', function(d){return d.cy})
   .attr('r', function(d){return d.r})
   .attr('fill', function(d){return d.fill})
+  .transition()
+    .duration(20000).ease('linear')
+    .attr('cx', function(d) {return d.transition.x})
+    .attr('cy', function(d) {return d.transition.y})
+    .each('end', randomTransition);
+
+function randomTransition() {
+  var dots = svg.selectAll('enemies')
+  dots.transition()
+    .duration(100000).ease('linear')
+    .attr('cx', function(d) {return d.transition.x})
+    .attr('cy', function(d) {return d.transition.y});
+}
 
 // Set initial radius of user
 var userRadius = 10;
@@ -64,13 +83,20 @@ d3.select('svg').on("mousemove", function () {
     eY = enemy.cy.baseVal.value;
 
     if(checkCollision(uX, uY, eX, eY, userRadius)) {
-      // Remove enemy where collision check is true
-      enemy.remove();
-      // Increase userRadius upon collision by 1/2 the radius of enemy
-      userRadius += 0.5 * enemy.r.baseVal.value;
-      // Update user object with updated radius
-      user.attr('r', userRadius)
-      console.log("current user radius: ", user[0][0].r.baseVal.value);
+      // If smaller than enemy, you lose
+      if(userRadius < enemy.r.baseVal.value) {
+          alert("you lose")
+      }
+      // Else eat enemy
+      else {
+        // Remove enemy where collision check is true
+        enemy.remove();
+        // Increase userRadius upon collision by 1/5 the radius of enemy
+        userRadius += 0.20 * enemy.r.baseVal.value;
+        // Update user object with updated radius
+        user.attr('r', userRadius)
+        console.log("current user radius: ", user[0][0].r.baseVal.value);
+      }
     }
   })
  });
@@ -78,3 +104,8 @@ d3.select('svg').on("mousemove", function () {
 function checkCollision(uX, uY, eX, eY, r){
   return Math.pow((uX - eX),2) + Math.pow((uY - eY),2) < Math.pow(r, 2)
 }
+
+
+// Next steps
+  // Have user circle follow a path
+  // Collision check does not fire until circle is there
